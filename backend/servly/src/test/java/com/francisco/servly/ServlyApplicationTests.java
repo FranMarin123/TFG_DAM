a0ccfe643c1159863fa9302fa9c281ef6f50454f
Francisco <franciscomarinalvarez@hotmail.es>
2025-05-12 20:58:23 +0200

    init: Correccion en nombre del proyecto del back

---
+++ backend/servly/src/test/java/com/francisco/servly/ServlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.servly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
